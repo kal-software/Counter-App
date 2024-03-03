b56be169ac899c9243e43cc0caaad2ede584e6b4
kalkidan-solomon-coder <152845891+kalkidan-solomon-coder@users.noreply.github.com>
2024-03-04 02:07:47 +0300

    second commit

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function Counter(){
-    const [count,setCount] = useState(0)
-    function increaseCount(){
-setCount(count+1)
-    }
-    function decreaseCount(){
-         setCount(count-1)
-    }
-            function resetCount(){
-                setCount(0)
-                    }
-return(
+function Counter() {
+  const [count, setCount] = useState(0);
+  function increaseCount() {
+    setCount(count + 1);
+  }
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+  function resetCount() {
+    setCount(0);
+  }
+  return (
     <div className="count-div">
-        <h1 className="counter-h1">Lets count</h1>
-        <h1 className="counter-h11">{count}</h1>
-        <div className="counter-button">
-        <button className="increase-button" onClick={increaseCount}>Increase</button>
-        <button className="decrease-button" onClick={decreaseCount}>Decrease</button>
-        <button className="reset-button" onClick={resetCount}>Reset</button>
-        </div>
-
-    
+      <h1 className="counter-h1">Lets count</h1>
+      <h1 className="counter-h11">{count}</h1>
+      <div className="counter-button">
+        <button className="increase-button" onClick={increaseCount}>
+          Increase
+        </button>
+        <button className="decrease-button" onClick={decreaseCount}>
+          Decrease
+        </button>
+        <button className="reset-button" onClick={resetCount}>
+          Reset
+        </button>
+      </div>
     </div>
-)
+  );
 }
 
-export default Counter
+export default Counter;
